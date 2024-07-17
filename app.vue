a747d0d39e9138873493414033ab1d0d7dffd875
s368952 <markus.ebner@stud-mail.uni-wuerzburg.de>
2024-07-17 20:22:38 +0200

    Add darkMode toggle via ref

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-   <h1>Twitter clone </h1>
+  <!-- dark is a class that depends on ref darkMode -->
+  <div :class="{'dark': darkMode }">
+   <h1 class="text-red-500 dark:text-dim-500">Twitter clone </h1>
   </div>
 </template>
+<script setup>
+const darkMode = ref(true)
+</script>
