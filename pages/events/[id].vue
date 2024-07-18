886b465485409c26817503560f06421adb3db59b
s368952 <markus.ebner@stud-mail.uni-wuerzburg.de>
2024-07-18 10:48:59 +0200

    Finish pages tutorial

---
+++ pages/events/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- [] for dynamic routes -->
+        event id: {{ $route.params.id }}
+    </div>
+</template>
+
+
+<script setup>
+
+const nuxt = useNuxtApp();
+const route = useRoute();
+console.log(route.params)
+console.log(nuxt)
+
+
+</script>
